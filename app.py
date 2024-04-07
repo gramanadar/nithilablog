46f75c24d9e4afaa42872c0a7550a36424399d15
gramanadar <165984559+gramanadar@users.noreply.github.com>
2024-04-06 20:51:54 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def main():
+    st.title('Nithila Blog')
+
+    # Wave Hand to Visitor
+    st.write("👋 Hello there! Welcome to my personal blog.")
+
+    # Introduce myself to visitors
+    st.write("I'm Nithila, and I'm thrilled to have you here!")
+
+    # Purpose of this blog
+    st.write("The purpose of this blog is to share my thoughts, experiences, and insights on various topics that interest me. "
+             "From technology and science to personal development and travel adventures, "
+             "I aim to provide valuable content and engage with my readers.")
+
+if __name__ == "__main__":
+    main()
